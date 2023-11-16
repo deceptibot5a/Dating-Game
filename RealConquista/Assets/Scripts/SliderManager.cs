c52008a2266a57eae4135e1f772fe5894a3165b6
deceptibot5a <deceptibot5a@gmail.com>
2023-11-16 11:41:18 -0500

    sistema de puntos

---
+++ RealConquista/Assets/Scripts/SliderManager.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using UnityEngine.UI;
+
+public class SliderManager : MonoBehaviour
+{
+    public Flowchart flowchart;
+    public string nombreVariable;
+    public Slider miSlider;
+    object valorVariable;
+
+    void Update() {
+        valorVariable = flowchart.GetVariable(nombreVariable).GetValue();
+        //Debug.Log((float)valorVariable);
+        miSlider.value = Convert.ToSingle(valorVariable)*0.01f;
+        Debug.Log(miSlider.value);
+    }
+}
